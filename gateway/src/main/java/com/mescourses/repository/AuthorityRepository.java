27db60932b61bc9a2f2989555ac42622d7095120
Unknown <vincent.turrin@gmail.com>
2017-11-07 10:05:55 +0100

    New project

---
+++ gateway/src/main/java/com/mescourses/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.mescourses.repository;
+
+import com.mescourses.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ gateway/src/main/java/com/mescourses/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.mescourses.service.dto;
